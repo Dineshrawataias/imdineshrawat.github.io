167d567b3d5125ec732cab78b83f11fd4d625a87
ImDR <dineshrawataias@gmail.com>
2019-05-30 09:35:37 +0530

    Vizexperts assignment added

---
+++ vizexperts-assignment/precache-manifest.43eb598e8b7e87aade7474dfb3d1702d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04d4f2d451fae31996f6b6a765dc236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2409ec05357db90a542",
+    "url": "/static/css/main.fd0b1f62.chunk.css"
+  },
+  {
+    "revision": "ba036fd862e2c7d1cf77",
+    "url": "/static/js/2.272d2254.chunk.js"
+  },
+  {
+    "revision": "d2409ec05357db90a542",
+    "url": "/static/js/main.bb1c4d5c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
