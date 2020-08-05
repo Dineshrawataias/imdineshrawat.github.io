774556afbfe43b1a020f3c78f5af4a6f5855ac98
Dinesh Rawat <Dineshrawataias@gmail.com>
2020-08-05 13:59:45 +0530

    Add files via upload

---
+++ mintu-app/precache-manifest.697833c844c20e9e6f3ae35c45a77efe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208426bd61035dd9ed3b406814d2a2fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6674651270e6cbdd8ce9",
+    "url": "/static/css/main.c6bf9587.chunk.css"
+  },
+  {
+    "revision": "48fc7db0af70492a3ada",
+    "url": "/static/js/2.37c63510.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.37c63510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6674651270e6cbdd8ce9",
+    "url": "/static/js/main.5f73c124.chunk.js"
+  },
+  {
+    "revision": "dcee968c3e0a001f08a0",
+    "url": "/static/js/runtime-main.3cb2813b.js"
+  }
+]);
